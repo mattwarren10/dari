47ac0047ff24ebf66b55db35cbbaf3fb37481135
mattwarren10 <mwarren@harding.edu>
2020-04-29 18:20:07 -0400

    add validations to sentence model

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,3 +1,8 @@
 class Sentence < ApplicationRecord
   belongs_to :word
+
+  # validates :english_sentence, presence: true, uniqueness: true
+  # validates :transliteration_sentence, presence: true, uniqueness: true
+  # validates :translation_sentence, presence: true, uniqueness: true
+  # validates :pronunciation_sentence, presence: true, uniqueness: true
 end
