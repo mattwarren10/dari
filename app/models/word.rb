2fefe50fc8c55cc4472da84b6fc91983cc00b631
mattwarren10 <mwarren@harding.edu>
2020-04-29 13:32:14 -0400

    comment out validations

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,10 +2,10 @@ class Word < ApplicationRecord
 	has_one_attached :pronunciation
 	has_one_attached :image
 
-	validates :english_word, uniqueness: true
-	validates :part_of_speech, uniqueness: true
-	validates :transliteration, uniqueness: true
-	validates :translation, uniqueness: true
-	validates :pronunciation, uniqueness: true
-	validates :image, uniqueness: true
+	# validates :english_word, uniqueness: true
+	# validates :part_of_speech, uniqueness: true
+	# validates :transliteration, uniqueness: true
+	# validates :translation, uniqueness: true
+	# validates :pronunciation, uniqueness: true
+	# validates :image, uniqueness: true
 end
